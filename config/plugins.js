a189c81719100d4772f757667105b91b0bd77818
Masambu Emmanuel <masambue11@gmail.com>
2024-05-27 14:04:25 +0300

    Rectify the server error not loading by adding API Keys

--- config/plugins.js
+++ config/plugins.js
@@ -1,15 +1,4 @@
 module.exports = () => ({
-  
+
 });
 
-// module.exports = {
-//   'transfer': {
-//     enabled: true,
-//     config: {
-//       auth: {
-//         // Use your preferred secret key here
-//         secret: process.env.TRANSFER_TOKEN_SECRET,
-//       },
-//     },
-//   },
-// };
